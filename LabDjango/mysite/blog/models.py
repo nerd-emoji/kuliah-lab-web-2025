471fb728b01647e595625f787bf37157386298e1
nerd-emoji <nathaniellouish@gmail.com>
2025-09-11 16:16:59 +0700

    django

---
+++ LabDjango/mysite/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Mahasiswa(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    nim = models.CharField(max_length=20)
+    jurusan = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.nim
---
+++ LabDjango/mysite/blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Mahasiswa
+from django.template import loader
+from django.http import HttpResponse
+
+# Create your views here.
+def mahasiswa_list(request):
+    mymahasiswa = Mahasiswa.objects.all().values()
+    template = loader.get_template('mahasiswa.html')
+    
+    context = {
+        'mymahasiswa': mymahasiswa,
+    }
+    return HttpResponse(template.render(context, request))
